4af2da03ec6cbbd754ebe528b19c5e8739d2d6fd
rusakroman <rrusak87@gmail.com>
2023-04-29 20:09:58 +0300

    Добавление второго товара во втором скрипте

---
+++ lesson_01/script2.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const productName = 'auto';
+const productCount = 6;
+const productCategory = 'Ferrari';
+const productPrice = 15000000;
+
+const productTotalPrice = productCount * productPrice;
+
+console.log('productName: ', productName);
+
+console.log(productTotalPrice);
