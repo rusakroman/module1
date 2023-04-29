23be03273ac28faf2e8b7c9e4ce4720f872764b0
rusakroman <rrusak87@gmail.com>
2023-04-29 20:03:21 +0300

    инициализация проекта

---
+++ lesson_01/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const productName = 'printer';
+const productCount = 10;
+const productCategory = 'HP';
+const productPrice = 8000;
+
+const productTotalPrice = productCount * productPrice;
+
+console.log('productName: ', productName);
+
+console.log(productTotalPrice);
