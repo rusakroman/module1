da637387b1cbfba44008a108566c40de3ab3c4ca
rusakroman <rrusak87@gmail.com>
2023-05-03 22:01:26 +0300

    внес изменения , удалил второй скрипт, дописал блок в первый

--- lesson_01/script.js
+++ lesson_01/script.js
@@ -10,3 +10,14 @@ const productTotalPrice = productCount * productPrice;
 console.log('productName: ', productName);
 
 console.log(productTotalPrice);
+
+const productName2 = 'auto';
+const productCount2 = 6;
+const productCategory2 = 'Ferrari';
+const productPrice2 = 15000000;
+
+const productTotalPrice2 = productCount2 * productPrice2;
+
+console.log('productName2: ', productName2);
+
+console.log(productTotalPrice2);
