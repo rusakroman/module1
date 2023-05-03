5c09a30b6d05aabd6968dd6a1f41530d1618a034
rusakroman <rrusak87@gmail.com>
2023-05-03 22:05:36 +0300

    create new folder in repository

---
+++ lesson_01/lesson_03/README.md
@@ -0,0 +1,10 @@
+# git-config
+
+git config --list
+git config --global user.name “имя”
+git config --global user.email “почта”
+
+
+git config --global core.safecrlf warn
+git config --global core.quotepath off
+git config --global init.defaultBranch main # Ветка по умолчанию
---
+++ lesson_01/lesson_03/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const productName = 'printer';
+const productCount = 10;
+const productCategory = 'HP';
+const productPrice = 8000;
+
+const productTotalPrice = productCount * productPrice;
+
+console.log('productName: ', productName);
+
+console.log(productTotalPrice);
+
+const productName2 = 'auto';
+const productCount2 = 6;
+const productCategory2 = 'Ferrari';
+const productPrice2 = 15000000;
+
+const productTotalPrice2 = productCount2 * productPrice2;
+
+console.log('productName2: ', productName2);
+
+console.log(productTotalPrice2);
