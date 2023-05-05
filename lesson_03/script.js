e1a023d286d833c16ef6c2802f52f33ce7440eab
rusakroman <rrusak87@gmail.com>
2023-05-05 11:14:09 +0300

    Исправил блоки инструкций

--- lesson_03/script.js
+++ lesson_03/script.js
@@ -1,42 +1,35 @@
 'use strict';
 
-const productName = 'printer';
-const productCount = 10;
-const productCategory = 'HP';
-const productPrice = 8000;
+{
+  const productName = 'printer';
+  const productCount = 10;
+  const productCategory = 'HP';
+  const productPrice = 8000;
+  const productTotalPrice = productCount * productPrice;
+  console.log('productName: ', productName);
+  console.log(productTotalPrice);
+}
 
-const productTotalPrice = productCount * productPrice;
-
-console.log('productName: ', productName);
-
-console.log(productTotalPrice);
-
-const productName2 = 'auto';
-const productCount2 = 6;
-const productCategory2 = 'Ferrari';
-const productPrice2 = 15000000;
-
-const productTotalPrice2 = productCount2 * productPrice2;
-
-console.log('productName2: ', productName2);
-
-console.log(productTotalPrice2);
+{
+  const productName = 'auto';
+  const productCount = 6;
+  const productCategory = 'Ferrari';
+  const productPrice = 15000000;
+  const productTotalPrice = productCount * productPrice;
+  console.log('productName: ', productName);
+  console.log(productTotalPrice);
+}
 
 // Новый блок инструкций!!!!
 
-const productUserName = prompt('Введите наименование товара', '');
-const productUserCount = +prompt('Введите количество товара', '');
-const productUserPrice = +prompt('Введите Цену товара', '');
-
-console.log(typeof productUserName);
-console.log(typeof productUserCount);
-console.log(typeof productUserPrice);
-
-const productUserTotalPrice = productUserCount * productUserPrice;
-
-console.log(`На складе ${productUserCount} единицы товара ${productUserName} на сумму ${productUserTotalPrice} деревянных`);
-
-// Задача номер 2
-
-
+{
+  const productName = prompt('Введите наименование товара', '');
+  const productCount = +prompt('Введите количество товара', '');
+  const productPrice = +prompt('Введите Цену товара', '');
+  console.log(typeof productName);
+  console.log(typeof productCount);
+  console.log(typeof productPrice);
+  const productTotalPrice = productCount * productPrice;
+  console.log(`На складе ${productCount} единицы товара ${productName} на сумму ${productTotalPrice} деревянных`);
+}
 
