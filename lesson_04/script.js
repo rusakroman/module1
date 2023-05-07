568c31e0a79920589456e76ea49990334988e409
rusakroman <rrusak87@gmail.com>
2023-05-07 21:29:56 +0300

    снова переделал задачу 1 модуля 4

--- lesson_04/script.js
+++ lesson_04/script.js
@@ -1,19 +1,19 @@
 'use strict';
 
+// Задача #1
+
 {
   const productName = prompt('Введите наименование товара', '');
-  const productCount = prompt('Введите количество товара', '');
-  const productPrice = prompt('Введите Цену товара', '');
+  const productCount = +prompt('Введите количество товара', '');
+  const productPrice = +prompt('Введите Цену товара', '');
   console.log(typeof productName);
   console.log(typeof productCount);
   console.log(typeof productPrice);
   const productTotalPrice = productCount * productPrice;
 
-  if ((isNaN(productCount)) || (isNaN(productPrice))) {
+  if ((Number.isNaN(productCount)) || (Number.isNaN(productPrice))) {
     console.log("Вы ввели некорректные данные");
   } else {
     console.log(`На складе ${productCount} единицы товара ${productName} на сумму ${productTotalPrice} деревянных`);
   }
-
 }
-
