69659f5d06a59442edfb9f311c04da8422795941
rusakroman <rrusak87@gmail.com>
2023-05-07 22:02:06 +0300

    решил основные три задачи

---
+++ lesson_04/tasks.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// Задача номер 2
+
+{
+  const rain = Math.round(Math.random() * 1);
+
+  if (rain === 1) {
+    console.log('Пошёл дождь. Возьмите зонт!');
+  } else if (rain === 0) {
+    console.log('Нет дождя');
+  }
+
+}
+
+
+
+// Задача номер 3
+
+{
+
+  const markMathem = +prompt('Введите кол-во балов по математике');
+  const markRash = +prompt('Введите кол-во балов по математике');
+  const markInform = +prompt('Введите кол-во балов по математике');
+
+  const markTotal = markMathem + markRash + markInform;
+
+  if ((Number.isNaN(markMathem)) || Number.isNaN(markRash) || Number.isNaN(markInform)) {
+    console.log("Вы ввели некорректные данные");
+  }
+
+  else if (markTotal >= 265) {
+    console.log('Вы поступили на бюджет!');
+  } else {
+    console.log('Увы, вы не прошли на бюджет!');
+  }
+
+}
