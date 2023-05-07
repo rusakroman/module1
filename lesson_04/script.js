c0e2d56b272a078513558850d90d8eeba7924cd5
rusakroman <rrusak87@gmail.com>
2023-05-07 20:27:22 +0300

    переделал 1 задачу 4 модуля с проверкой на NaN

--- lesson_04/script.js
+++ lesson_04/script.js
@@ -2,16 +2,18 @@
 
 {
   const productName = prompt('Введите наименование товара', '');
-  const productCount = +prompt('Введите количество товара', '');
-  const productPrice = +prompt('Введите Цену товара', '');
+  const productCount = prompt('Введите количество товара', '');
+  const productPrice = prompt('Введите Цену товара', '');
   console.log(typeof productName);
   console.log(typeof productCount);
   console.log(typeof productPrice);
   const productTotalPrice = productCount * productPrice;
 
-  if (Number.isNaN(productTotalPrice)) {
-    console.log('Вы ввели некорректные данные!');
+  if ((isNaN(productCount)) || (isNaN(productPrice))) {
+    console.log("Вы ввели некорректные данные");
   } else {
     console.log(`На складе ${productCount} единицы товара ${productName} на сумму ${productTotalPrice} деревянных`);
   }
+
 }
+
