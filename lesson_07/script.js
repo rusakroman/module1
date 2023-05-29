31de0502f08dc4341fe5158b74e0c73cedff2ed2
rusakroman <rrusak87@gmail.com>
2023-05-29 20:28:01 +0300

    Первая задача массивы

--- lesson_07/script.js
+++ lesson_07/script.js
@@ -1,28 +1,8 @@
 "use strict";
 
-// const animals = ['dog', 'cat', 'mouse', 'wolf'];
+const allStudents = ['Иванов', 'Петров', 'Сидоров', 'Кузнецов', 'Смирнов', 'Попов', 'Соколов'];
 
-// const printArr = arr => {
-//   console.log(`Длина массива ${arr.length}`);
-//   for (let i = 0; i < arr.length; i++) {
-//     const elem = arr[i];
-//     console.log(`Элемент под индексом ${i} - ${elem}`);
-//   }
-
-//   for (const key in arr) {
-//     console.log(`Элемент под индексом ${key} - ${arr[key]}`);
-//   }
-// }
-
-// printArr(animals);
-
-
-
-// console.log(Array.isArray(animals));
-
-const students = ['Ivanov', 'Sidorov', 'Petrov', 'Liushin', 'Mamonowa'];
-
-const neudStudents = ['Sidorov', 'Petrov'];
+const neudStudents = ['Сидоров', 'Смирнов', 'Попов'];
 
 const goodStudents = [];
 
@@ -34,62 +14,3 @@ for (let i = 0; i < students.length; i++) {
     }
   }
 }
-
-// const neud = students.splice(2, 1, 'Haustow', 'Chernikova');
-
-// const numbers = [1, 23, 4, 121, 2112, 3546, 24, 23];
-
-// console.log(students);
-
-// console.log(neud);
-
-// console.log(students.includes('Liushin'));
-
-// console.log(students.join(' '));
-
-// const sortArr = arr => {
-//   console.log(`Изначальный массив: ${arr}`);
-//   arr.sort((a, b) => a - b);
-//   console.log(`Отсортированный массив ${arr}`);
-//   return arr.reverse();
-// };
-
-// sortArr(numbers);
-// console.log(students.reverse());
-// console.log('sortArr(numbers) ', sortArr(numbers));
-
-// const allStudents = ['Иванов', 'Петров', 'Сидоров', 'Кузнецов', 'Смирнов', 'Попов', 'Соколов'];
-
-// const failedStudents = ['Сидоров', 'Смирнов', 'Попов',];
-
-// const successStudents = [];
-
-// const foo = (arr1, arr2) => {
-//   for (let i = 0; i < allStudents.length; i++) {
-//     for (let j = 0; j < failedStudents.length; j++) {
-//       if (failedStudents[j] !== allStudents[i]) {
-//         successStudents.push(allStudents[i]);
-//       }
-//     }
-//   }
-//   return successStudents;
-
-// };
-
-// const result = foo(allStudents, failedStudents);
-// console.log(result);
-
-// const names = ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
-
-// const addPrefix = (arr, prefix) => {
-//   for (let i = 0; i < names.length; i++) {
-//     console.log('Mr ' + names[i]);
-//   }
-//   return
-
-// }
-
-// addPrefix(names, 'Mr');
-
-// const newNames = addPrefix(names, 'Mr');
-// console.log(newNames);
