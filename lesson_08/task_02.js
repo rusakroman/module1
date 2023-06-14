85963d41d9340228b735b15c6c280322ba7e3082
rusakroman <rrusak87@gmail.com>
2023-06-14 16:52:13 +0300

    вторая задача

--- lesson_08/task_02.js
+++ lesson_08/task_02.js
@@ -13,14 +13,18 @@ const foo = (length, n, m) => {
   if (n < m) {
     max = m;
   }
-  if (n === m) {
-    alert('выберите неодинаковые числа');
-  }
+
+  if (n === m) return arrRandomNumber;
+
+
   for (let i = 0; i < length; i++) {
+    if (n === m) {
+      break;
+    }
     arr.push(Math.floor(Math.random() * (Math.max(n, m) - Math.min(n, m) + 1)) + Math.min(n, m));
   }
   return arr;
 }
 
-const result = foo(22, -10, -30);
+const result = foo(22, -10, -10);
 console.log(result);
