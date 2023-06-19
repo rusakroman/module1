ddd6b2c940439a07a19920a4c08747651e416ff4
rusakroman <rrusak87@gmail.com>
2023-06-19 10:44:28 +0300

    решил три задачи

--- lesson_08/task_02.js
+++ lesson_08/task_02.js
@@ -1,30 +1,31 @@
 "use strict";
 
-const arr = [];
-let min = 0;
-let max = 0;
+{
+  const arr = [];
+  let min = 0;
+  let max = 0;
 
-const foo = (length, n, m) => {
+  const foo = (length, n, m) => {
 
-  if (n > m) {
-    max = n;
-  }
-
-  if (n < m) {
-    max = m;
-  }
-
-  if (n === m) return arrRandomNumber;
-
-
-  for (let i = 0; i < length; i++) {
-    if (n === m) {
-      break;
+    if (n > m) {
+      max = n;
     }
-    arr.push(Math.floor(Math.random() * (Math.max(n, m) - Math.min(n, m) + 1)) + Math.min(n, m));
+
+    if (n < m) {
+      max = m;
+    }
+
+    for (let i = 0; i < length; i++) {
+      if (n === m) {
+        break;
+      }
+      arr.push(Math.floor(Math.random() * (Math.max(n, m) - Math.min(n, m) + 1)) + Math.min(n, m));
+    }
+    return arr;
   }
-  return arr;
+
+  const result = foo(22, -10, -20);
+  console.log(result);
+
 }
 
-const result = foo(22, -10, -10);
-console.log(result);
---
+++ lesson_08/task_03.js
@@ -0,0 +1,48 @@
+"use strict";
+
+{
+  const ourArray = [];
+  let min;
+  let max;
+  let m;
+  let n;
+
+
+
+  const foo = (length, m, n, str) => {
+    if (str !== 'even' && str !== 'odd') {
+      alert('Иди нахер!');
+      return ourArray;
+    }
+
+    if (m > n) {
+      max = m;
+      min = n;
+    }
+    else {
+      max = n;
+      min = m;
+    }
+
+    for (let i = 0; i < length; i++) {
+      let c = Math.floor(min + Math.random() * (max + 1 - min));
+      if (str === 'even' && (c % 2 === 0)) {
+        ourArray.push(c);
+        continue;
+      }
+      if (str === 'odd' && (c % 2 !== 0)) {
+        ourArray.push(c);
+        continue;
+      }
+      length++;
+    }
+
+
+    return ourArray;
+  }
+
+  const result = foo(12, 5, 15, 'even');
+  console.log(result);
+
+}
+
